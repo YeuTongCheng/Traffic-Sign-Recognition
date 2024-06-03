2a896b3fbb76c3ceb045804817315534654687f3
Nicole <103835914+YeuTongCheng@users.noreply.github.com>
2024-06-02 20:45:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-# **Team 43: Final Project - German Traffic Sign Recognition Benchmark (GTSRB)**
-***Business problem***: Electric vehicles have undergone significant advancements, with a notable
-emphasis on intelligence as a key driver for their development. One prominent trend contributing
-to this progress is the integration of autonomous driving systems, which are increasingly
-becoming core components of electric vehicles. Recognizing that environmental perception
-serves as the foundation for intelligent planning and secure decision-making in autonomous
-vehicles, our focus is on advancing the innovation of traffic signs recognition. This enhancement
-aims to elevate the reliability and safety of automated vehicles by bolstering their ability to
-perceive and respond to traffic signs.
-
-***Data Source***: The German Traffic Sign Recognition Benchmark (GTSRB) --- The official training and
-test dataset contains images and annotations, three sets of different HOG features and Haar-like
-features, Hue histograms
-
-  # Import libraries
+# Data Preprocessing
+from google.colab import drive
+drive.mount('/content/drive') # where I store the training data
+# Import libraries
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
