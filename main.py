e957c0d26d2bff26c85c27c0bdfd837a9c8972ad
Nicole <103835914+YeuTongCheng@users.noreply.github.com>
2024-06-09 11:07:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -401,3 +401,33 @@ def make_fig_cm(cm):
 
 cm = confusion_matrix(all_classes,predictions)
 make_fig_cm(cm)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # convolutional layer (sees 32x32x3 image tensor)
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        # convolutional layer (sees 16x16x16 tensor)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        # convolutional layer (sees 8x8x32 tensor)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        # linear layer (64 * 4 * 4 -> 500)
+        self.fc1 = nn.Linear(64 * 4 * 4, 500)
+        # linear layer (500 -> 43)
+        self.fc2 = nn.Linear(500, 43)
+        # dropout layer (p=0.25)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
